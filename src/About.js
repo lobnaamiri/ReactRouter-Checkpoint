ba8f942edcf68924ad486266c0f95b284f79b06c
lobnaamiri <amirilobna2015@gmail.com>
2019-08-30 11:30:21 +0100

    first commit

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class About extends Component{
+    render(){
+        return(
+            <>
+                    
+             <h3>About page</h3>
+             
+            </>
+        )
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import About from './About';
+import Contact from './Contact';
+import Home from './Home';
 import './App.css';
+import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>head</h3>
+      <Router>
+        <div className="nav">
+          {}
+          <h3>Menu</h3>
+          <Link to="/">Home</Link>&nbsp;&nbsp;
+          <Link to="/About">About</Link>&nbsp;&nbsp;
+          <Link to="/contact">Contact</Link>
+        
+        </div> 
+        <div className="affich">
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route  path="/About" component={About}/>
+            <Route path="/contact" component={Contact}/>
+          </Switch>
+        </div>
+        <h3>footer</h3>
+       
+      </Router>
     </div>
   );
 }
